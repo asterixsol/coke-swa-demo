a89ed3d62c0a5d7dbc267162a5ddc745226f2c1f
Zartab M Nakhwa <zartab@codewithz.com>
2022-12-22 12:58:06 +0400

    Test 2

--- coke-swa-demo/src/main/java/com/example/demo/controller/HelloController.java
+++ coke-swa-demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello Spring Boot App";
+		return "Hello Spring Boot App!!!";
 	}
 	
 	@GetMapping("/hello1")
